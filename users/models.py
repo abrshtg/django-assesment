bfe7ab1554b9e17f1928b3812a17ad0b7462d0a5
abrshtg <abrshtgam@gmail.com>
2024-10-22 20:37:07 +0300

    create a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+ROLE_CHOICES = (
+    ("admin", "Admin"),
+    ("coach", "Coach"),
+    ("agent", "Agent"),
+    ("football_player", "Football Player"),
+)
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, role=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("role", "admin")
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("role") != "admin":
+            raise ValueError("Superuser must have role as Admin.")
+
+        return self.create_user(email, password=password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["role"]
+
+    def __str__(self):
+        return self.email
